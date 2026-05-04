185b0d96d5981ef2c3f95ab54579a56a5c825245
ploymahloy <patrick.mahloy@gmail.com>
2026-05-03 22:24:50 -0400

    Add audio i/o device detection
    Console:
    Host: CoreAudio
      Input devices:
        MacBook Pro Microphone
      Output devices:
        MacBook Pro Speakers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use cpal::traits::{DeviceTrait, HostTrait};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    for host_id in cpal::available_hosts() {
+        println!("Host: {}", host_id.name());
+
+        let host = cpal::host_from_id(host_id)?;
+
+        println!("  Input devices:");
+        match host.input_devices() {
+            Ok(devices) => {
+                for device in devices {
+                    match device.description() {
+                        Ok(desc) => println!("    {}", desc),
+                        Err(e) => println!("    <error reading device: {}>", e),
+                    }
+                }
+            }
+            Err(e) => println!("    <error enumerating: {}>", e),
+        }
+
+        println!("  Output devices:");
+        match host.output_devices() {
+            Ok(devices) => {
+                for device in devices {
+                    match device.description() {
+                        Ok(desc) => println!("    {}", desc),
+                        Err(e) => println!("    <error reading device: {}>", e),
+                    }
+                }
+            }
+            Err(e) => println!("    <error enumerating: {}>", e),
+        }
+
+        println!();
+    }
+
+    Ok(())
 }
